5c37dbc9b6b9ded57beabb740f1213cb45ba6ee7
Lucia Villa <lu.villavega@gmail.com>
2024-02-28 00:50:20 -0300

    primer commit lu, eventos reales creados

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".slick-carousel").slick({
+    slidesToShow: 1,
+    autoplay: true, // Activar reproducción automática
+    autoplaySpeed: 2000, // Velocidad de transición (en milisegundos)
+    arrows: false, // Ocultar flechas de navegación (puedes personalizarlas)
+    dots: true, // Mostrar indicadores de navegación (puedes personalizarlos)
+  });
+});
